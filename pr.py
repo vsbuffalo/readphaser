bc621a1b6b419cbf9d175488565414f0c9213c53
Vince Buffalo <vsbuffalo@gmail.com>
2013-02-09 12:50:59 -0800

    changed order to allow for -1 tids

--- pr.py
+++ pr.py
@@ -297,9 +297,10 @@ def phase_reads(bam_filename, hapcut_file, unphased_file, inconsistent_counts_fi
     if unphased_file is not None:
         unphased_contigs = set(bamfile.references) - set(hapcut_dict.keys())
         for read in bamfile:
+            if (read.is_unmapped or read.mapq < mapq or (exclude_duplicates and read.is_duplicate)):
+                continue
             readname = bamfile.getrname(read.tid)
-            if (readname not in unphased_contigs or read.is_unmapped or
-                read.mapq < mapq or (exclude_duplicates and read.is_duplicate)):
+            if readname not in unphased_contigs:
                 continue
             which_read = 1 if read.is_read1 else 2
             seq = read.query
